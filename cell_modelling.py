cc2aebfeaa879052791df51e4018e0f9fb6a3f9d
wanaga3166 <benoit-aliaga@orange.fr>
2019-05-01 18:31:56 +0200

    Premier commit

---
+++ cell_modelling.py
@@ -0,0 +1,28 @@
+class Cell():
+  def __init__(self, x, y):
+    self.x = 5 
+    self.y = 10
+    self.xnuc = None
+    self.ynuc = None
+
+
+  def get_nuclear(self):
+#    if self.xnuc is None or self.ynuc is None:
+#      raise Exception("You should have called obj.nuclear to set nuc value before trying to get Them.")
+    return (self.xnuc, self.ynuc)
+
+  def set_nuclear(self, x, y):
+    self.xnuc = x
+    self.ynuc = y
+
+  def cell_area(self, x, y):
+    self_area = x * y 
+
+class Protein():
+  def __init__(self):
+    pass
+
+class Diffusion():
+  def __init__(self):
+    pass
+
