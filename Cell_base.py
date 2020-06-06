acb84399dbbd15f128184d13174e734ed74d6f44
wanaga3166 <benoit-aliaga@orange.fr>
2020-06-06 21:41:43 +0200

    I removed the cell class and I started to write the protein class

--- Cell_base.py
+++ Cell_base.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
 
 """
 Cell_base.py
@@ -12,33 +12,51 @@ __contact__ = ("aliaga.benoit@gmail.com")
 __version__ = "0.1"
 __date__ = "06/06/2020"
 
+import math
+from tkinter import *
 
+#class Cell:
+#    def __init__(self, length, width):
+#        """
+#           Initialisation d'un objet
+#           Definition des attributs avec des valeurs par defauts.
+#        """
+#        self.x = length
+#        self.y = width
 
-class Cell:
-    def __init__(self, length, width):
-        """
-           Intialisation d'un objet
-           Definition des attributs avec des valeurs par defauts.
-        """
-        self.x = length
-        self.y = width
+#    def cell_area(self):
+#        """
+#           Methode qui calcule la surface de la cellule.
+#        """
+#        area = self.x * self.y
+#        return area
 
-    def cell_area(self):
-        """
-           Methode qui calcule la surface de la cellule.
-        """
-        area = self.x * self.y
-        return area
+#    def nuclear_(self):
+#        """
+#           Dessin d'un canvas
+#        """
 
 class Protein():
-    def __init__(self, pos_x, pos_y):
+    def __init__(self, pos_x, pos_y, vx, vy, mw):
         """
-           Classe qui definie une proteine
+           Classe qui defini une proteine
         """
-        self.coordx = pos_x
-        self.coordy = pos_y
+        self.coord_x = pos_x
+        self.coord_y = pos_y
+        self.vx = vx
+        self.vy = vy
+        self.mw = mw
 
-    def prot_coord(self):
+    def init_position(self):
+        """
+           Position of the protein
+        """
+        return("The cell coordinates are: x = {} and y = {}".format(self.coord_x, self.coord_y)) 
+
+    def nuclear_area(self):
+        pass
+    
+    def coordo(self):
         """
            Methode qui calcule les coordonnees de la proteine.
         """
@@ -47,9 +65,13 @@ class Protein():
 
 # Main section
 if __name__ == "__main__":
-    my_cell = Cell(2,1)
-    print("Width = ", my_cell.x, "Length = ", my_cell.y)
-    print("The cell area is:", my_cell.cell_area())
-    my_prot = Protein(4,6)
-    print("The coordinate of my protein is:", my_prot.prot_coord())
+    my_prot = Protein(2,1,4,5,10)
+    print(my_prot)
+    print(my_prot.init_position())
     
+    #my_cell = Cell(2,1)
+    #print("Width = ", my_cell.x, "Length = ", my_cell.y)
+    #print("The cell area is:", my_cell.cell_area())
+    #my_prot = Protein(4,6)
+    #print("The coordinate of my protein is:", my_prot.prot_coord())
+
